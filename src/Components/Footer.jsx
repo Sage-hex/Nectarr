ee376c2902b66de0db117038c1d07ff449da3d73
Nuelsage (Kosisochukwu) <74513233+Sage-hex@users.noreply.github.com>
2024-09-07 18:39:28 +0100

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,77 @@
+import {useEffect} from 'react'
+import './footer.css'
+// import { FaCircleUser } from "react-icons/fa6";
+import { FaCameraRetro } from "react-icons/fa";
+import { AiOutlineX } from "react-icons/ai";
+import { FaWhatsapp } from "react-icons/fa";
+import Logo from '../assets/Images/signup-logo.png'
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
+const Footer = () => {
+    useEffect(()=>{
+        Aos.init()
+      },[])
+  return (
+    <footer data-aos="fade-down" className='footer'>
+      <div className="wrapper">
+        <div className="description">
+          <div>
+            <img src={Logo} alt="" className='logo'/>
+          </div>
+
+          <section className='instruction'>
+              <div className="Intouch">
+                <h3>GET IN TOUCH</h3>
+                <p>Plot 2055, Green Estate, Amuwo Odofin,
+                Lagos.</p>
+                <p>+2347062810942</p>
+                <p>info@nectrabuzz.com</p>
+                <span>
+                <FaCameraRetro fontSize={'20px'} color='white'/>
+                <AiOutlineX fontSize={'20px'} color='white'/>
+                <FaWhatsapp fontSize={'20px'} color='white'/>
+                </span>
+              </div>
+
+
+              
+
+              <div className="Intouch">
+                <h3>INFORMATION</h3>
+                <p>About Us</p>
+                <p>Our Blog</p>
+                <p>Privacy Policy</p>
+                <p>Terms & Condition</p>
+              </div>
+
+
+              <div className="Intouch">
+                <h3>SUPPORT</h3>
+                <p>FAQs</p>
+                <p>Track Order</p>
+                <p>Jar Size Guide</p>
+                <p>Return Policy</p>
+                <p>Shipping & Delivery</p>
+              </div>
+
+          </section>
+
+        </div>
+        <p>NectarBuzz Nigeria, Designed by Group 5 Zephora Nigeria, Designed by Group 1 </p>
+      </div>
+
+
+
+      
+     
+      {/* GET IN TOUCH
+
+   
+
+*/}
+    </footer>
+  )
+}
+
+export default Footer
