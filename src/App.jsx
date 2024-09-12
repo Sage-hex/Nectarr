4db8111c1db5d6f492f19a1529836f3ee3b18fef
Kosisochukwu <realitynuel@gmail.com>
2024-09-12 07:47:15 +0100

    resolved routing err

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-<<<<<<< HEAD
 import router from './Components/Routes/Routes';
-=======
-import router from './Components/Routes';
->>>>>>> main
-
 const App = () => (
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
 );
 
 export default App;
