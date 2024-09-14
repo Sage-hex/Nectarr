58abeef2f3592c23e9a1ff76f4e7cbcb21f95732
Kosisochukwu <realitynuel@gmail.com>
2024-09-14 19:20:22 +0100

    updating the aboutpage

--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from 'react'
 import './About.css'
-import honey1 from '../../assets/Images/Nectar-Buzz/honey1.png'
-
+import aboutHero from '../../assets/Images/about-hero-bg.png'
 import HoneySection from '../../Components/HoneySection/HoneySection'
-
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import AboutUs from '../../Components/AboutUs/AboutUs'
@@ -11,6 +9,7 @@ import ChooseUs from '../../Components/ChooseUs/ChooseUs'
 import OurBeekeepers from '../../Components/OurBeeKeepers/OurBeekeepers'
 import DoYouKnow from '../../Components/DoYouKnow/DoYouKnow'
 import NewsLetter from '../../Components/NewsLetter/NewsLetter'
+import Button from '../../Components/Button/Button'
 
 
 const About = () => {
@@ -20,19 +19,9 @@ const About = () => {
     return (
         <div className='AboutPage'>
         <div className="About">
-           <div className="hero">
-                <img src={honey1} alt="" className='honey1'/>
-             <div className="heroLeft">
-                   <div className="heroMessage">
-                      <h1>Nature's <span>Golden</span><span>Buzz</span></h1>
-                      <h1>100% Organic</h1>
-                      <button className='shopnow'>shop Now</button>
-                   </div>
-              </div>
-              <div className="heroLeft">
-                  
-              </div>
-           </div>
+        <h1 className='hero-text'>We dey <span>BUZZ</span> <br/> FOR YOU <br/>MY<span>GUY</span> <br/><span>WELL WELL</span></h1>
+        <Button>shop Now</Button>
+                      
         </div>
     
         <AboutUs/>
