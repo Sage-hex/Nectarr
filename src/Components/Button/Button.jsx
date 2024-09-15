a428b777fd96970968f4b6913b032d5f26afe3a1
Kosisochukwu <realitynuel@gmail.com>
2024-09-15 10:41:04 +0100

    working on the about responsiveness

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,6 +2,28 @@ import "./Button.css"
 
 const Button = ({ children, onClick }) => {
 
+    // const Button = ({ 
+    //     children, 
+    //     onClick, 
+    //     className = '', 
+    //     type = 'button', 
+    //     disabled = false 
+    // }) => {
+    //     return (
+    //         <button 
+    //             className={`Button ${className}`} 
+    //             onClick={onClick} 
+    //             type={type} 
+    //             disabled={disabled}
+    //         >
+    //             {children}
+    //         </button>
+    //     );
+    // };
+
+    // export default Button;
+
+
 
     return (
         <button className="Button Button-custom" onClick={onClick}>{children}</button>
--- src/Components/DoYouKnow/DoYouKnow.jsx
+++ src/Components/DoYouKnow/DoYouKnow.jsx
@@ -1,60 +1,53 @@
-import React from 'react'
-import honey1 from '../../assets/Images/Nectar-Buzz/honey1.png'
+import React from 'react';
+import honey1 from '../../assets/Images/Nectar-Buzz/honey1.png';
 import HoneyDrop from '../../assets/Images/Nectar-Buzz/HoneyDrop.png';
 import Drop from '../../assets/Images/Nectar-Buzz/Drop.png';
 import honeypot from '../../assets/Images/Nectar-Buzz/honeypot.png';
-import beeYourSelf from '../../assets/Images/Nectar-Buzz/input.png'
-import './DoYouKnow.css'
+import beeYourSelf from '../../assets/Images/Nectar-Buzz/input.png';
+import Button from '../Button/Button'
+import './DoYouKnow.css';
 
 const DoYouKnow = () => {
-    return ( <
-        >
+    return (
         <div className="DoYouKnow">
             <h1>Did You Know</h1>
             <div className="Honeykick">
                 <div className="HoneyCon">
                     <h2>Honey kicks off COLD</h2>
-                    <img src={Drop} alt="" className='honeyjar'/>
-                    <h3>Honey kicks off COLD <br />
-                      1 tablespoon of honey <br />
-                      1 tablespoon of lemon juice</h3>
+                    <img src={honeypot} alt="" className='honeyjar'/>
+                    <h3>Mix 1 tablespoon of honey with 1 tablespoon of lemon juice.</h3>
                 </div>
 
                 <div className="HoneyCon">
                     <h2>Honey for DETOX</h2>
                     <img src={HoneyDrop} alt="" className='honeyjar'/>
-                    <h3>
-                    1 cup of herbal tea <br />
-                    1-2 cup of honey</h3>
+                    <h3>For a detox, add 1-2 cups of honey to a cup of herbal tea.</h3>
                 </div>
 
                 <div className="HoneyCon">
                     <h2>Honey for SORE THROAT</h2>
-                    <img src={honeypot} alt="" className='honeyjar'/>
+                    <img src={Drop} alt="" className='honeyjar'/>
+                    <h3>Honey soothes a sore throat when mixed in warm water.</h3>
                 </div>
             </div>
 
-        </div>
+            <div className="HFT">
+                <div className="HoneyCon">
+                    <h2>Honey for Toothache</h2>
+                    <img src={honey1} alt="" className='honeyjar'/>
+                    <h3>Mix 1 teaspoon of cinnamon with 1 teaspoon of honey.</h3>
+                </div>
 
-
-        <
-        div className = "HFT" >
-        <div className="HoneyCon">
-                <h2>Honey for Toothache</h2>
-                <img src={honey1} alt="" className='honeyjar'/>
-                <h3>
-                1 teaspon of cinnamon <br />
-                1 teaspoon of honwy</h3>
+                <div className="newsletterForm">
+                    {/*<h2>Bee Yourself</h2>*/}
+                    <form>
+                        <input type="email" placeholder="Enter your email" required />
+                        <Button type="submit">Subscribe</Button>
+                    </form>
+                </div>
             </div>
-
-        <
-        img src = { beeYourSelf } alt = ""
-        className = ' beeYourSelf' / >
-
-        <
-        /div> <
-        />
-    )
+        </div>
+    );
 }
 
-export default DoYouKnow
+export default DoYouKnow;
