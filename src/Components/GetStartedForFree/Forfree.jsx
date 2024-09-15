0f8850471b757eecc6d580316a350c89b7703434
Kosisochukwu <realitynuel@gmail.com>
2024-09-15 18:22:45 +0100

    working on the homepage responsiveness

--- src/Components/GetStartedForFree/Forfree.jsx
+++ src/Components/GetStartedForFree/Forfree.jsx
@@ -4,9 +4,9 @@ import bro from '../../assets/Images/Nectar-Buzz/bro.png'
 import { useNavigate } from 'react-router-dom'
 
 const Forfree = () => {
-  const nav = useNavigate()
-  return (
-    <div className='Forfree'>
+    const nav = useNavigate()
+    return (
+        <div className='Forfree'>
       <div className="ForfreeHead">
         <h1>Get Started for FREE</h1>
         <p>Select account type to get Started</p>
@@ -14,19 +14,19 @@ const Forfree = () => {
 
       <div className="GetStartedBox">
          <div className="MainBox">
-            <img src={bro} alt=""  className='GetStartedImage'/>
-            <button className='btn' onClick={()=>nav('/signup')}>CUSTOMER</button>
+            <img src={bro} alt="Customer"  className='GetStartedImage'/>
+            <button className='btn' onClick={() => nav('/signup')}>CUSTOMER</button>
             <p>Earn as you Shop</p>
             <p>Get Points and be Redeemed✨</p>
          </div>
          <div className="MainBox">
-            <img src={bro} alt=""  className='GetStartedImage'/>
-            <button className='btn' onClick={()=> nav('/beekeeperSignup')}>BEEKEEPER</button>
+            <img src={bro} alt="Beekeeper"  className='GetStartedImage'/>
+            <button className='btn' onClick={() => nav('/beekeeperSignup')}>BEEKEEPER</button>
             <p>Get across to million honey Lovers💘</p>
          </div>
       </div>
     </div>
-  )
+    )
 }
 
-export default Forfree
+export default Forfree;
--- src/Components/GetStartedHead/GetStartedHead.jsx
+++ src/Components/GetStartedHead/GetStartedHead.jsx
@@ -3,15 +3,15 @@ import './getStartedHead.css'
 import { Link } from 'react-router-dom'
 import getLogo from '../../assets/Images/signup-logo.png'
 
-const getStartedHead = () => {
-  return (
-    <div className='getStartedHead'>
+const GetStartedHead = () => {
+    return (
+        <div className='getStartedHead'>
       <div className="getStartedHeadWrap">
-        <img src={getLogo} alt="" className='getLogo '/>
-        <p>Existing Customer?<Link to={'/login'} style={{color:'white'}}> Login</Link> </p>
+        <img src={getLogo} alt="Signup Logo" className='getLogo'/>
+        <p>Existing Customer? <Link to={'/login'} style={{color:'white'}}>Login</Link></p>
       </div>
     </div>
-  )
+    )
 }
 
-export default getStartedHead
+export default GetStartedHead;
